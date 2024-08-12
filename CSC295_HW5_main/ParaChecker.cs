a87a19f8ab83234125439432d9f6be9e736ee970
jbowen999 <julian.e.bowen@outlook.com>
2024-08-12 14:44:46 -0700

    Hopefully I created this repository correctly...

---
+++ CSC295_HW5_main/ParaChecker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC295_HW5
+{
+    public class ParaChecker
+    {
+        public static Stack<char> createStack(string inputString)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                stack.Push(inputString[i]);
+            }
+            return stack;
+        }
+        public static bool IsBalanced(Stack<char> stack)
+        {
+            bool isCurly = true;
+            bool isBracket = true;
+            bool isPara = true;
+            while (stack.Any())
+            {
+                char value = stack.Pop();
+                if (value == '}' || value == '{')
+                {
+                    isCurly = !isCurly;
+                }
+                else if (value == ']' || value == '[')
+                {
+                    isBracket = !isBracket;
+                }
+                else if (value == ')' || value == '(')
+                {
+                    isPara = !isPara;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (isCurly && isBracket && isPara) return true;
+            else return false;
+        }
+    }
+}
---
+++ CSC295_HW5_main/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CSC295_HW5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Stack<char> chars = ParaChecker.createStack("(HelloWorld!)");
+            Console.WriteLine(  chars.Peek());
+            Console.WriteLine(ParaChecker.IsBalanced(chars));
+        }
+        
+    }
+}
